29dd512bac19019c59480547ef8e2e41cfc247db
d-floutier <dualengine@gmail.com>
2025-09-04 20:33:20 +0200

    Implement Second Video

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./sections/Hero";
 import NavBar from "./sections/NavBar";
 import FirstVideo from "./sections/FirstVideo";
 import Jason from "./sections/Jason";
+import SecondVideo from "./sections/SecondVideo";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -15,6 +16,7 @@ const App = () => {
         <Hero />
         <FirstVideo />
         <Jason />
+        <SecondVideo />
       </div>
     </main>
   );
